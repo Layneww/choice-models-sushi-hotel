a292e6e8b9c056526acfa7739aa0a20444761fc7
Layneww <layne.w@foxmail.com>
2023-09-24 19:32:52 -0500

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version='0.0.1',
     packages=setuptools.find_packages(),
     include_package_data=True,
-    install_requires=['numpy', 'numba', 'scipy', 'ghalton', 'matplotlib'],
+    install_requires=['numpy', 'numba', 'scipy', 'matplotlib'],
     python_requires='>=3.6',
     zip_safe=False
 )
